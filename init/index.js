37aed94c4406040c936fb248b75a5f745fae2c32
Sanskar-dev786 <pandey1606sanskar@gmail.com>
2025-03-30 08:08:36 +0530

    Basic templating of this project is completed

--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const initdata = require("./data.js");
+const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -18,7 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  await Listing.insertMany(initdata.data);
+  await Listing.insertMany(initData);
   console.log("data was initialized");
 };
 
